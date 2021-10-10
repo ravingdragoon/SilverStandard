83d26a19fe3b98ff0185d9b9b71c847e5fcf4d2f
ravingdragoon <72875320+ravingdragoon@users.noreply.github.com>
2021-10-10 09:52:38 -0700

    Update SilverStandard.js

--- SilverStandard.js
+++ SilverStandard.js
@@ -29,25 +29,3 @@ Hooks.on('renderItemSheet5eWithBags', (sheet, html) => {
     html.find('[name="data.currency.ep"]').remove();
     html.find('"Electrum"').remove();
 });
-
-
-Hooks.once('ready', () => {
-  CONFIG.Actor.sheetClasses.character['dnd5e.ActorSheet5eCharacter'].cls.prototype._onConvertCurrency = _onMyConvertCurrency;
-});
-
-  function _onMyConvertCurrency(event) {
-    event.preventDefault();
-    const curr = duplicate(this.actor.data.data.currency);
-    console.log(curr);
-    const convert = {
-      cp: {into: "sp", each: 10},
-      sp: {into: "gp", each: 10},
-      gp: {into: "pp", each: 10}
-    };
-    for ( let [c, t] of Object.entries(convert) ) {
-      let change = Math.floor(curr[c] / t.each);
-      curr[c] -= (change * t.each);
-      curr[t.into] += change;
-    }
-    return this.actor.update({"data.currency": curr});
- };
