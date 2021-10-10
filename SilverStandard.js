3eef67cbdc46bf3dd361971eb35c10b1ee907b36
ravingdragoon <72875320+ravingdragoon@users.noreply.github.com>
2021-10-10 10:00:04 -0700

    Update SilverStandard.js

--- SilverStandard.js
+++ SilverStandard.js
@@ -8,6 +8,11 @@ Hooks.on('renderTidy5eSheet', (sheet, html) => {
     html.find('.denomination.ep').remove();
     html.find('[name="data.currency.ep"]').remove();
 });
+Hooks.on('renderTidy5eNPC', (sheet, html) => {
+    html.find('.denomination.ep').parent().remove();
+    html.find('.denomination.ep').remove();
+    html.find('[name="data.currency.ep"]').remove();
+});
 Hooks.on('renderDNDBeyondCharacterSheet5e', (sheet, html) => {
     html.find('.denomination.ep').remove();
     html.find('[name="data.currency.ep"]').remove();
